6de99532fa22af506799f365935ac35138867256
bukebuchi <247364041@qq.com>
2019-07-19 10:56:53 +0800

    1

---
+++ app/src/main/java/com/baidu/track/model/LoginnewBean.java
@@ -0,0 +1,161 @@
+package com.wind.carmanager.model;
+
+public class LoginnewBean extends HttpResponse{
+    /**
+     * code : 1
+     * msg : 登录成功
+     * time : 1562835444
+     * data : {"userinfo":{"id":2,"username":"13212384180","nickname":"13212384180","mobile":"13212384180","avatar":"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgaGVpZ2h0PSIxMDAiIHdpZHRoPSIxMDAiPjxyZWN0IGZpbGw9InJnYigyMTIsMTYwLDIyOSkiIHg9IjAiIHk9IjAiIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj48L3JlY3Q+PHRleHQgeD0iNTAiIHk9IjUwIiBmb250LXNpemU9IjUwIiB0ZXh0LWNvcHk9ImZhc3QiIGZpbGw9IiNmZmZmZmYiIHRleHQtYW5jaG9yPSJtaWRkbGUiIHRleHQtcmlnaHRzPSJhZG1pbiIgYWxpZ25tZW50LWJhc2VsaW5lPSJjZW50cmFsIj4xPC90ZXh0Pjwvc3ZnPg==","score":0,"token":"8e616be1-69b2-410a-b7d6-23717cbbbce8","user_id":2,"createtime":1562835444,"expiretime":1565427444,"expires_in":2592000}}
+     */
+
+    private String time;
+    private DataBean data;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        /**
+         * userinfo : {"id":2,"username":"13212384180","nickname":"13212384180","mobile":"13212384180","avatar":"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgaGVpZ2h0PSIxMDAiIHdpZHRoPSIxMDAiPjxyZWN0IGZpbGw9InJnYigyMTIsMTYwLDIyOSkiIHg9IjAiIHk9IjAiIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj48L3JlY3Q+PHRleHQgeD0iNTAiIHk9IjUwIiBmb250LXNpemU9IjUwIiB0ZXh0LWNvcHk9ImZhc3QiIGZpbGw9IiNmZmZmZmYiIHRleHQtYW5jaG9yPSJtaWRkbGUiIHRleHQtcmlnaHRzPSJhZG1pbiIgYWxpZ25tZW50LWJhc2VsaW5lPSJjZW50cmFsIj4xPC90ZXh0Pjwvc3ZnPg==","score":0,"token":"8e616be1-69b2-410a-b7d6-23717cbbbce8","user_id":2,"createtime":1562835444,"expiretime":1565427444,"expires_in":2592000}
+         */
+
+        private UserinfoBean userinfo;
+
+        public UserinfoBean getUserinfo() {
+            return userinfo;
+        }
+
+        public void setUserinfo(UserinfoBean userinfo) {
+            this.userinfo = userinfo;
+        }
+
+        public static class UserinfoBean {
+            /**
+             * id : 2
+             * username : 13212384180
+             * nickname : 13212384180
+             * mobile : 13212384180
+             * avatar : data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgaGVpZ2h0PSIxMDAiIHdpZHRoPSIxMDAiPjxyZWN0IGZpbGw9InJnYigyMTIsMTYwLDIyOSkiIHg9IjAiIHk9IjAiIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj48L3JlY3Q+PHRleHQgeD0iNTAiIHk9IjUwIiBmb250LXNpemU9IjUwIiB0ZXh0LWNvcHk9ImZhc3QiIGZpbGw9IiNmZmZmZmYiIHRleHQtYW5jaG9yPSJtaWRkbGUiIHRleHQtcmlnaHRzPSJhZG1pbiIgYWxpZ25tZW50LWJhc2VsaW5lPSJjZW50cmFsIj4xPC90ZXh0Pjwvc3ZnPg==
+             * score : 0
+             * token : 8e616be1-69b2-410a-b7d6-23717cbbbce8
+             * user_id : 2
+             * createtime : 1562835444
+             * expiretime : 1565427444
+             * expires_in : 2592000
+             */
+
+            private int id;
+            private String username;
+            private String nickname;
+            private String mobile;
+            private String avatar;
+            private int score;
+            private String token;
+            private int user_id;
+            private int createtime;
+            private int expiretime;
+            private int expires_in;
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getUsername() {
+                return username;
+            }
+
+            public void setUsername(String username) {
+                this.username = username;
+            }
+
+            public String getNickname() {
+                return nickname;
+            }
+
+            public void setNickname(String nickname) {
+                this.nickname = nickname;
+            }
+
+            public String getMobile() {
+                return mobile;
+            }
+
+            public void setMobile(String mobile) {
+                this.mobile = mobile;
+            }
+
+            public String getAvatar() {
+                return avatar;
+            }
+
+            public void setAvatar(String avatar) {
+                this.avatar = avatar;
+            }
+
+            public int getScore() {
+                return score;
+            }
+
+            public void setScore(int score) {
+                this.score = score;
+            }
+
+            public String getToken() {
+                return token;
+            }
+
+            public void setToken(String token) {
+                this.token = token;
+            }
+
+            public int getUser_id() {
+                return user_id;
+            }
+
+            public void setUser_id(int user_id) {
+                this.user_id = user_id;
+            }
+
+            public int getCreatetime() {
+                return createtime;
+            }
+
+            public void setCreatetime(int createtime) {
+                this.createtime = createtime;
+            }
+
+            public int getExpiretime() {
+                return expiretime;
+            }
+
+            public void setExpiretime(int expiretime) {
+                this.expiretime = expiretime;
+            }
+
+            public int getExpires_in() {
+                return expires_in;
+            }
+
+            public void setExpires_in(int expires_in) {
+                this.expires_in = expires_in;
+            }
+        }
+    }
+}
